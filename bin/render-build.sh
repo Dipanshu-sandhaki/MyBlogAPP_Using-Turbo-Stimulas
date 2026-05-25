5785a1331d2a1a7d53b8d01d715a6504496fadd5
Dipanshu <dipanshuroy6@gmail.com>
2026-05-25 11:25:07 +0000

    fix render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,9 @@
-cat > bin/render-build.sh << 'EOF'
 #!/usr/bin/env bash
 set -o errexit
 
+bundle binstubs bundler --force
 bundle install
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-EOF
