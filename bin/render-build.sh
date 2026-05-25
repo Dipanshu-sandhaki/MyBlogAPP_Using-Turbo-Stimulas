1cdfd6c77f12b089c520ad8379b36f5f50200dcb
Dipanshu <dipanshuroy6@gmail.com>
2026-05-25 11:35:16 +0000

    remove broken bin/bundle, fix build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,11 @@
+cat > bin/render-build.sh << 'EOF'
 #!/usr/bin/env bash
 set -o errexit
 
-bundle binstubs bundler --force
+gem install bundler --no-document
 bundle install
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+EOF
