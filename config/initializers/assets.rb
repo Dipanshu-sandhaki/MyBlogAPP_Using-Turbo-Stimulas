ca0a327b756d4b5bbdb6bb4e6ebc0c26872dcc3b
Dipanshu <dipanshuroy6@gmail.com>
2026-05-25 10:44:23 +0000

    update production config and fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,23 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  application.js
+  application.css
+  controllers/application.js
+  controllers/index.js
+  controllers/bulk_select_controller.js
+  controllers/bulk_upload_controller.js
+  controllers/blog_editor_controller.js
+  controllers/comment_edit_controller.js
+  controllers/darkmode_controller.js
+  controllers/dropdown_controller.js
+  controllers/flash_controller.js
+  controllers/form_controller.js
+  controllers/hello_controller.js
+  controllers/modal_controller.js
+  controllers/read_more_controller.js
+]
