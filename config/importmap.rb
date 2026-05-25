e7d9a25808d2f468e48fdc317b44cc110b00d830
Dipanshu <dipanshuroy6@gmail.com>
2026-05-25 12:29:31 +0000

    Fix missing importmap packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 
-pin "chart.js"
-pin "@kurkle/color", to: "@kurkle--color.js"pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
+pin "chart.js" # @4.5.1
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.4.0
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.300
